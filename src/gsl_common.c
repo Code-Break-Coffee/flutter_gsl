f16abbd59f5fdea08c2e4d4c6b30d1bee8edf53c
Kartikey2011yadav <nykay10968@gmail.com>
2025-01-04 19:24:32 +0530

    constants

---
+++ src/gsl_common.c
@@ -0,0 +1,7 @@
+#include "gsl_common.h"
+#include <math.h>
+
+/* gsl_finite is a wrapper for the standard library function isfinite */
+int gsl_finite(double x) {
+    return isfinite(x);
+}
--- src/gsl_const.h
+++ src/gsl_const.h
@@ -2,109 +2,227 @@
 #define GSL_CONST_H
 
 // Constants    ----------------------------------------------------------------
-
-// Physical Constants
-extern const double GSL_CONST_MKSA_SPEED_OF_LIGHT;
-extern const double GSL_CONST_MKSA_GRAVITATIONAL_CONSTANT;
-extern const double GSL_CONST_MKSA_PLANCKS_CONSTANT_H;
-extern const double GSL_CONST_MKSA_PLANCKS_CONSTANT_HBAR;
-extern const double GSL_CONST_MKSA_BOLTZMANN;
-extern const double GSL_CONST_MKSA_STEFAN_BOLTZMANN_CONSTANT;
-extern const double GSL_CONST_MKSA_THOMSON_CROSS_SECTION;
-extern const double GSL_CONST_NUM_AVOGADRO;
+// Numerical Constants
 extern const double GSL_CONST_NUM_FINE_STRUCTURE;
+extern const double GSL_CONST_NUM_AVOGADRO;
+extern const double GSL_CONST_NUM_YOTTA;
+extern const double GSL_CONST_NUM_ZETTA;
+extern const double GSL_CONST_NUM_EXA;
+extern const double GSL_CONST_NUM_PETA;
+extern const double GSL_CONST_NUM_TERA;
+extern const double GSL_CONST_NUM_GIGA;
+extern const double GSL_CONST_NUM_MEGA;
+extern const double GSL_CONST_NUM_KILO;
+extern const double GSL_CONST_NUM_MILLI;
+extern const double GSL_CONST_NUM_MICRO;
+extern const double GSL_CONST_NUM_NANO;
+extern const double GSL_CONST_NUM_PICO;
+extern const double GSL_CONST_NUM_FEMTO;
+extern const double GSL_CONST_NUM_ATTO;
+extern const double GSL_CONST_NUM_ZEPTO;
+extern const double GSL_CONST_NUM_YOCTO;
 
-// Units of Measurement
-extern const double GSL_CONST_MKSA_ACRE;
-extern const double GSL_CONST_MKSA_ANGSTROM;
-extern const double GSL_CONST_MKSA_ASTRONOMICAL_UNIT;
-extern const double GSL_CONST_MKSA_BAR;
-extern const double GSL_CONST_MKSA_BARN;
-extern const double GSL_CONST_MKSA_BOHR_MAGNETON;
-extern const double GSL_CONST_MKSA_BOHR_RADIUS;
-extern const double GSL_CONST_MKSA_BTU;
-extern const double GSL_CONST_MKSA_CALORIE;
-extern const double GSL_CONST_MKSA_CANADIAN_GALLON;
-extern const double GSL_CONST_MKSA_CARAT;
-extern const double GSL_CONST_MKSA_CURIE;
-extern const double GSL_CONST_MKSA_DAY;
-extern const double GSL_CONST_MKSA_DEBYE;
-extern const double GSL_CONST_MKSA_DYNE;
-extern const double GSL_CONST_MKSA_ELECTRON_CHARGE;
-extern const double GSL_CONST_MKSA_ELECTRON_MAGNETIC_MOMENT;
-extern const double GSL_CONST_MKSA_ELECTRON_VOLT;
-extern const double GSL_CONST_MKSA_ERG;
-extern const double GSL_CONST_MKSA_FARADAY;
-extern const double GSL_CONST_MKSA_FATHOM;
-extern const double GSL_CONST_MKSA_FOOT;
-extern const double GSL_CONST_MKSA_FOOTCANDLE;
-extern const double GSL_CONST_MKSA_FOOTLAMBERT;
-extern const double GSL_CONST_MKSA_GAUSS;
-extern const double GSL_CONST_MKSA_GRAM_FORCE;
-extern const double GSL_CONST_MKSA_GRAV_ACCEL;
-extern const double GSL_CONST_MKSA_HECTARE;
-extern const double GSL_CONST_MKSA_HORSEPOWER;
-extern const double GSL_CONST_MKSA_HOUR;
-extern const double GSL_CONST_MKSA_INCH;
-extern const double GSL_CONST_MKSA_INCH_OF_MERCURY;
-extern const double GSL_CONST_MKSA_INCH_OF_WATER;
-extern const double GSL_CONST_MKSA_JOULE;
-extern const double GSL_CONST_MKSA_KILOMETERS_PER_HOUR;
-extern const double GSL_CONST_MKSA_KILOPOUND_FORCE;
-extern const double GSL_CONST_MKSA_KNOT;
-extern const double GSL_CONST_MKSA_LAMBERT;
-extern const double GSL_CONST_MKSA_LIGHT_YEAR;
-extern const double GSL_CONST_MKSA_LITER;
-extern const double GSL_CONST_MKSA_LUMEN;
-extern const double GSL_CONST_MKSA_LUX;
-extern const double GSL_CONST_MKSA_MASS_ELECTRON;
-extern const double GSL_CONST_MKSA_MASS_MUON;
-extern const double GSL_CONST_MKSA_MASS_NEUTRON;
-extern const double GSL_CONST_MKSA_MASS_PROTON;
-extern const double GSL_CONST_MKSA_METER_OF_MERCURY;
-extern const double GSL_CONST_MKSA_METRIC_TON;
-extern const double GSL_CONST_MKSA_MICRON;
-extern const double GSL_CONST_MKSA_MIL;
-extern const double GSL_CONST_MKSA_MILE;
-extern const double GSL_CONST_MKSA_MILES_PER_HOUR;
-extern const double GSL_CONST_MKSA_MINUTE;
-extern const double GSL_CONST_MKSA_MOLAR_GAS;
-extern const double GSL_CONST_MKSA_NAUTICAL_MILE;
-extern const double GSL_CONST_MKSA_NEWTON;
-extern const double GSL_CONST_MKSA_NUCLEAR_MAGNETON;
-extern const double GSL_CONST_MKSA_OUNCE_MASS;
-extern const double GSL_CONST_MKSA_PARSEC;
-extern const double GSL_CONST_MKSA_PHOT;
-extern const double GSL_CONST_MKSA_PINT;
-extern const double GSL_CONST_MKSA_POINT;
-extern const double GSL_CONST_MKSA_POISE;
-extern const double GSL_CONST_MKSA_POUND_FORCE;
-extern const double GSL_CONST_MKSA_POUND_MASS;
-extern const double GSL_CONST_MKSA_POUNDAL;
-extern const double GSL_CONST_MKSA_PROTON_MAGNETIC_MOMENT;
-extern const double GSL_CONST_MKSA_PSI;
-extern const double GSL_CONST_MKSA_QUART;
-extern const double GSL_CONST_MKSA_RAD;
-extern const double GSL_CONST_MKSA_ROENTGEN;
-extern const double GSL_CONST_MKSA_RYDBERG;
-extern const double GSL_CONST_MKSA_SOLAR_MASS;
-extern const double GSL_CONST_MKSA_STANDARD_GAS_VOLUME;
-extern const double GSL_CONST_MKSA_STD_ATMOSPHERE;
-extern const double GSL_CONST_MKSA_STILB;
-extern const double GSL_CONST_MKSA_STOKES;
-extern const double GSL_CONST_MKSA_TEXPOINT;
-extern const double GSL_CONST_MKSA_THERM;
-extern const double GSL_CONST_MKSA_TON;
-extern const double GSL_CONST_MKSA_TORR;
-extern const double GSL_CONST_MKSA_TROY_OUNCE;
-extern const double GSL_CONST_MKSA_UK_GALLON;
-extern const double GSL_CONST_MKSA_UK_TON;
-extern const double GSL_CONST_MKSA_UNIFIED_ATOMIC_MASS;
-extern const double GSL_CONST_MKSA_US_GALLON;
-extern const double GSL_CONST_MKSA_VACUUM_PERMEABILITY;
-extern const double GSL_CONST_MKSA_VACUUM_PERMITTIVITY;
-extern const double GSL_CONST_MKSA_WEEK;
-extern const double GSL_CONST_MKSA_YARD;
+// Physical Constants (MKSA)
+extern const double GSL_CONST_MKSA_SPEED_OF_LIGHT; /* m / s */
+extern const double GSL_CONST_MKSA_GRAVITATIONAL_CONSTANT; /* m^3 / kg s^2 */
+extern const double GSL_CONST_MKSA_PLANCKS_CONSTANT_H; /* kg m^2 / s */
+extern const double GSL_CONST_MKSA_PLANCKS_CONSTANT_HBAR; /* kg m^2 / s */
+extern const double GSL_CONST_MKSA_BOLTZMANN; /* kg m^2 / K s^2 */
+extern const double GSL_CONST_MKSA_STEFAN_BOLTZMANN_CONSTANT; /* kg / K^4 s^3 */
+extern const double GSL_CONST_MKSA_THOMSON_CROSS_SECTION; /* m^2 */
+
+// Units of Measurement (MKSA)
+extern const double GSL_CONST_MKSA_ACRE; /* m^2 */
+extern const double GSL_CONST_MKSA_ANGSTROM; /* m */
+extern const double GSL_CONST_MKSA_ASTRONOMICAL_UNIT; /* m */
+extern const double GSL_CONST_MKSA_BAR; /* kg / m s^2 */
+extern const double GSL_CONST_MKSA_BARN; /* m^2 */
+extern const double GSL_CONST_MKSA_BOHR_MAGNETON; /* A m^2 */
+extern const double GSL_CONST_MKSA_BOHR_RADIUS; /* m */
+extern const double GSL_CONST_MKSA_BTU; /* kg m^2 / s^2 */
+extern const double GSL_CONST_MKSA_CALORIE; /* kg m^2 / s^2 */
+extern const double GSL_CONST_MKSA_CANADIAN_GALLON; /* m^3 */
+extern const double GSL_CONST_MKSA_CARAT; /* kg */
+extern const double GSL_CONST_MKSA_CURIE; /* 1 / s */
+extern const double GSL_CONST_MKSA_DAY; /* s */
+extern const double GSL_CONST_MKSA_DEBYE; /* C m */
+extern const double GSL_CONST_MKSA_DYNE; /* m kg / s^2 */
+extern const double GSL_CONST_MKSA_ELECTRON_CHARGE; /* A s */
+extern const double GSL_CONST_MKSA_ELECTRON_MAGNETIC_MOMENT; /* A m^2 */
+extern const double GSL_CONST_MKSA_ELECTRON_VOLT; /* kg m^2 / s^2 */
+extern const double GSL_CONST_MKSA_ERG; /* kg m^2 / s^2 */
+extern const double GSL_CONST_MKSA_FARADAY; /* A s / mol */
+extern const double GSL_CONST_MKSA_FATHOM; /* m */
+extern const double GSL_CONST_MKSA_FOOT; /* m */
+extern const double GSL_CONST_MKSA_FOOTCANDLE; /* cd sr / m^2 */
+extern const double GSL_CONST_MKSA_FOOTLAMBERT; /* cd sr / m^2 */
+extern const double GSL_CONST_MKSA_GAUSS; /* T */
+extern const double GSL_CONST_MKSA_GRAM_FORCE; /* m kg / s^2 */
+extern const double GSL_CONST_MKSA_GRAV_ACCEL; /* m / s^2 */
+extern const double GSL_CONST_MKSA_HECTARE; /* m^2 */
+extern const double GSL_CONST_MKSA_HORSEPOWER; /* kg m^2 / s^3 */
+extern const double GSL_CONST_MKSA_HOUR; /* s */
+extern const double GSL_CONST_MKSA_INCH; /* m */
+extern const double GSL_CONST_MKSA_INCH_OF_MERCURY; /* kg / m s^2 */
+extern const double GSL_CONST_MKSA_INCH_OF_WATER; /* kg / m s^2 */
+extern const double GSL_CONST_MKSA_JOULE; /* kg m^2 / s^2 */
+extern const double GSL_CONST_MKSA_KILOMETERS_PER_HOUR; /* m / s */
+extern const double GSL_CONST_MKSA_KILOPOUND_FORCE; /* m kg / s^2 */
+extern const double GSL_CONST_MKSA_KNOT; /* m / s */
+extern const double GSL_CONST_MKSA_LAMBERT; /* cd sr / m^2 */
+extern const double GSL_CONST_MKSA_LIGHT_YEAR; /* m */
+extern const double GSL_CONST_MKSA_LITER; /* m^3 */
+extern const double GSL_CONST_MKSA_LUMEN; /* cd sr */
+extern const double GSL_CONST_MKSA_LUX; /* cd sr / m^2 */
+extern const double GSL_CONST_MKSA_MASS_ELECTRON; /* kg */
+extern const double GSL_CONST_MKSA_MASS_MUON; /* kg */
+extern const double GSL_CONST_MKSA_MASS_NEUTRON; /* kg */
+extern const double GSL_CONST_MKSA_MASS_PROTON; /* kg */
+extern const double GSL_CONST_MKSA_METER_OF_MERCURY; /* kg / m s^2 */
+extern const double GSL_CONST_MKSA_METRIC_TON; /* kg */
+extern const double GSL_CONST_MKSA_MICRON; /* m */
+extern const double GSL_CONST_MKSA_MIL; /* m */
+extern const double GSL_CONST_MKSA_MILE; /* m */
+extern const double GSL_CONST_MKSA_MILES_PER_HOUR; /* m / s */
+extern const double GSL_CONST_MKSA_MINUTE; /* s */
+extern const double GSL_CONST_MKSA_MOLAR_GAS; /* kg m^2 / K mol s^2 */
+extern const double GSL_CONST_MKSA_NAUTICAL_MILE; /* m */
+extern const double GSL_CONST_MKSA_NEWTON; /* m kg / s^2 */
+extern const double GSL_CONST_MKSA_NUCLEAR_MAGNETON; /* A m^2 */
+extern const double GSL_CONST_MKSA_OUNCE_MASS; /* kg */
+extern const double GSL_CONST_MKSA_PARSEC; /* m */
+extern const double GSL_CONST_MKSA_PHOT; /* cd sr / m^2 */
+extern const double GSL_CONST_MKSA_PINT; /* m^3 */
+extern const double GSL_CONST_MKSA_POINT; /* m */
+extern const double GSL_CONST_MKSA_POISE; /* kg / m s */
+extern const double GSL_CONST_MKSA_POUND_FORCE; /* m kg / s^2 */
+extern const double GSL_CONST_MKSA_POUND_MASS; /* kg */
+extern const double GSL_CONST_MKSA_POUNDAL; /* m kg / s^2 */
+extern const double GSL_CONST_MKSA_PROTON_MAGNETIC_MOMENT; /* A m^2 */
+extern const double GSL_CONST_MKSA_PSI; /* kg / m s^2 */
+extern const double GSL_CONST_MKSA_QUART; /* m^3 */
+extern const double GSL_CONST_MKSA_RAD; /* m^2 / s^2 */
+extern const double GSL_CONST_MKSA_ROENTGEN; /* A s / kg */
+extern const double GSL_CONST_MKSA_RYDBERG; /* kg m^2 / s^2 */
+extern const double GSL_CONST_MKSA_SOLAR_MASS; /* kg */
+extern const double GSL_CONST_MKSA_STANDARD_GAS_VOLUME; /* m^3 / mol */
+extern const double GSL_CONST_MKSA_STD_ATMOSPHERE; /* kg / m s^2 */
+extern const double GSL_CONST_MKSA_STILB; /* cd / m^2 */
+extern const double GSL_CONST_MKSA_STOKES; /* m^2 / s */
+extern const double GSL_CONST_MKSA_TEXPOINT; /* m */
+extern const double GSL_CONST_MKSA_THERM; /* kg m^2 / s^2 */
+extern const double GSL_CONST_MKSA_TON; /* kg */
+extern const double GSL_CONST_MKSA_TORR; /* kg / m s^2 */
+extern const double GSL_CONST_MKSA_TROY_OUNCE; /* kg */
+extern const double GSL_CONST_MKSA_UK_GALLON; /* m^3 */
+extern const double GSL_CONST_MKSA_UK_TON; /* kg */
+extern const double GSL_CONST_MKSA_UNIFIED_ATOMIC_MASS; /* kg */
+extern const double GSL_CONST_MKSA_US_GALLON; /* m^3 */
+extern const double GSL_CONST_MKSA_VACUUM_PERMEABILITY; /* H / m */
+extern const double GSL_CONST_MKSA_VACUUM_PERMITTIVITY; /* F / m */
+extern const double GSL_CONST_MKSA_WEEK; /* s */
+extern const double GSL_CONST_MKSA_YARD; /* m */
+
+// Physical Constants (CGSM)
+extern const double GSL_CONST_CGSM_SPEED_OF_LIGHT; /* cm / s */
+extern const double GSL_CONST_CGSM_GRAVITATIONAL_CONSTANT; /* cm^3 / g s^2 */
+extern const double GSL_CONST_CGSM_PLANCKS_CONSTANT_H; /* g cm^2 / s */
+extern const double GSL_CONST_CGSM_PLANCKS_CONSTANT_HBAR; /* g cm^2 / s */
+extern const double GSL_CONST_CGSM_BOLTZMANN; /* g cm^2 / K s^2 */
+extern const double GSL_CONST_CGSM_STEFAN_BOLTZMANN_CONSTANT; /* g / K^4 s^3 */
+extern const double GSL_CONST_CGSM_THOMSON_CROSS_SECTION; /* cm^2 */
+
+// Units of Measurement (CGSM)
+extern const double GSL_CONST_CGSM_ASTRONOMICAL_UNIT; /* cm */
+extern const double GSL_CONST_CGSM_LIGHT_YEAR; /* cm */
+extern const double GSL_CONST_CGSM_PARSEC; /* cm */
+extern const double GSL_CONST_CGSM_GRAV_ACCEL; /* cm / s^2 */
+extern const double GSL_CONST_CGSM_ELECTRON_VOLT; /* g cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_MASS_ELECTRON; /* g */
+extern const double GSL_CONST_CGSM_MASS_MUON; /* g */
+extern const double GSL_CONST_CGSM_MASS_PROTON; /* g */
+extern const double GSL_CONST_CGSM_MASS_NEUTRON; /* g */
+extern const double GSL_CONST_CGSM_RYDBERG; /* g cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_MOLAR_GAS; /* g cm^2 / K mol s^2 */
+extern const double GSL_CONST_CGSM_STANDARD_GAS_VOLUME; /* cm^3 / mol */
+extern const double GSL_CONST_CGSM_MINUTE; /* s */
+extern const double GSL_CONST_CGSM_HOUR; /* s */
+extern const double GSL_CONST_CGSM_DAY; /* s */
+extern const double GSL_CONST_CGSM_WEEK; /* s */
+extern const double GSL_CONST_CGSM_INCH; /* cm */
+extern const double GSL_CONST_CGSM_FOOT; /* cm */
+extern const double GSL_CONST_CGSM_YARD; /* cm */
+extern const double GSL_CONST_CGSM_MILE; /* cm */
+extern const double GSL_CONST_CGSM_NAUTICAL_MILE; /* cm */
+extern const double GSL_CONST_CGSM_FATHOM; /* cm */
+extern const double GSL_CONST_CGSM_MIL; /* cm */
+extern const double GSL_CONST_CGSM_POINT; /* cm */
+extern const double GSL_CONST_CGSM_TEXPOINT; /* cm */
+extern const double GSL_CONST_CGSM_MICRON; /* cm */
+extern const double GSL_CONST_CGSM_ANGSTROM; /* cm */
+extern const double GSL_CONST_CGSM_HECTARE; /* cm^2 */
+extern const double GSL_CONST_CGSM_ACRE; /* cm^2 */
+extern const double GSL_CONST_CGSM_BARN; /* cm^2 */
+extern const double GSL_CONST_CGSM_LITER; /* cm^3 */
+extern const double GSL_CONST_CGSM_US_GALLON; /* cm^3 */
+extern const double GSL_CONST_CGSM_QUART; /* cm^3 */
+extern const double GSL_CONST_CGSM_PINT; /* cm^3 */
+extern const double GSL_CONST_CGSM_CUP; /* cm^3 */
+extern const double GSL_CONST_CGSM_FLUID_OUNCE; /* cm^3 */
+extern const double GSL_CONST_CGSM_TABLESPOON; /* cm^3 */
+extern const double GSL_CONST_CGSM_TEASPOON; /* cm^3 */
+extern const double GSL_CONST_CGSM_CANADIAN_GALLON; /* cm^3 */
+extern const double GSL_CONST_CGSM_UK_GALLON; /* cm^3 */
+extern const double GSL_CONST_CGSM_MILES_PER_HOUR; /* cm / s */
+extern const double GSL_CONST_CGSM_KILOMETERS_PER_HOUR; /* cm / s */
+extern const double GSL_CONST_CGSM_KNOT; /* cm / s */
+extern const double GSL_CONST_CGSM_POUND_MASS; /* g */
+extern const double GSL_CONST_CGSM_OUNCE_MASS; /* g */
+extern const double GSL_CONST_CGSM_TON; /* g */
+extern const double GSL_CONST_CGSM_METRIC_TON; /* g */
+extern const double GSL_CONST_CGSM_UK_TON; /* g */
+extern const double GSL_CONST_CGSM_TROY_OUNCE; /* g */
+extern const double GSL_CONST_CGSM_CARAT; /* g */
+extern const double GSL_CONST_CGSM_UNIFIED_ATOMIC_MASS; /* g */
+extern const double GSL_CONST_CGSM_GRAM_FORCE; /* cm g / s^2 */
+extern const double GSL_CONST_CGSM_POUND_FORCE; /* cm g / s^2 */
+extern const double GSL_CONST_CGSM_KILOPOUND_FORCE; /* cm g / s^2 */
+extern const double GSL_CONST_CGSM_POUNDAL; /* cm g / s^2 */
+extern const double GSL_CONST_CGSM_CALORIE; /* g cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_BTU; /* g cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_THERM; /* g cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_HORSEPOWER; /* g cm^2 / s^3 */
+extern const double GSL_CONST_CGSM_BAR; /* g / cm s^2 */
+extern const double GSL_CONST_CGSM_STD_ATMOSPHERE; /* g / cm s^2 */
+extern const double GSL_CONST_CGSM_TORR; /* g / cm s^2 */
+extern const double GSL_CONST_CGSM_METER_OF_MERCURY; /* g / cm s^2 */
+extern const double GSL_CONST_CGSM_INCH_OF_MERCURY; /* g / cm s^2 */
+extern const double GSL_CONST_CGSM_INCH_OF_WATER; /* g / cm s^2 */
+extern const double GSL_CONST_CGSM_PSI; /* g / cm s^2 */
+extern const double GSL_CONST_CGSM_POISE; /* g / cm s */
+extern const double GSL_CONST_CGSM_STOKES; /* cm^2 / s */
+extern const double GSL_CONST_CGSM_STILB; /* cd / cm^2 */
+extern const double GSL_CONST_CGSM_LUMEN; /* cd sr */
+extern const double GSL_CONST_CGSM_LUX; /* cd sr / cm^2 */
+extern const double GSL_CONST_CGSM_PHOT; /* cd sr / cm^2 */
+extern const double GSL_CONST_CGSM_FOOTCANDLE; /* cd sr / cm^2 */
+extern const double GSL_CONST_CGSM_LAMBERT; /* cd sr / cm^2 */
+extern const double GSL_CONST_CGSM_FOOTLAMBERT; /* cd sr / cm^2 */
+extern const double GSL_CONST_CGSM_CURIE; /* 1 / s */
+extern const double GSL_CONST_CGSM_ROENTGEN; /* abamp s / g */
+extern const double GSL_CONST_CGSM_RAD; /* cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_SOLAR_MASS; /* g */
+extern const double GSL_CONST_CGSM_BOHR_RADIUS; /* cm */
+extern const double GSL_CONST_CGSM_NEWTON; /* cm g / s^2 */
+extern const double GSL_CONST_CGSM_DYNE; /* cm g / s^2 */
+extern const double GSL_CONST_CGSM_JOULE; /* g cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_ERG; /* g cm^2 / s^2 */
+extern const double GSL_CONST_CGSM_BOHR_MAGNETON; /* abamp cm^2 */
+extern const double GSL_CONST_CGSM_NUCLEAR_MAGNETON; /* abamp cm^2 */
+extern const double GSL_CONST_CGSM_ELECTRON_MAGNETIC_MOMENT; /* abamp cm^2 */
+extern const double GSL_CONST_CGSM_PROTON_MAGNETIC_MOMENT; /* abamp cm^2 */
+extern const double GSL_CONST_CGSM_FARADAY; /* abamp s / mol */
+extern const double GSL_CONST_CGSM_ELECTRON_CHARGE; /* abamp s */
 
 // Mathematical Constants
 extern const double M_E;
@@ -141,4 +259,4 @@ extern const double GSL_SET_COMPLEX;
 extern const double GSL_SIGN;
 
 
-#endif // GSL_CONST_H
+#endif // GSL_CONSTS_H
