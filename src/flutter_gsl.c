413ce082f0ae5860c93663533322507f0e0d95cb
Kartikey2011yadav <nykay10968@gmail.com>
2025-01-07 12:33:45 +0530

    restructuring

--- src/flutter_gsl.c
+++ src/flutter_gsl.c
@@ -1,25 +1,24 @@
 #include "flutter_gsl.h"
 
-#include "gsl_const.h"
 
-// Bessel functions
-double gsl_bessel_j0(double x) {
-    return gsl_sf_bessel_J0(x);
-}
-
-double gsl_bessel_j1(double x) {
-    return gsl_sf_bessel_J1(x);
-}
-
-double gsl_bessel_y0(double x) {
-    return gsl_sf_bessel_Y0(x);
-}
-
-double gsl_bessel_y1(double x) {
-    return gsl_sf_bessel_Y1(x);
-}
-
-
-double gsl_bessel_sin(double x){
-    return gsl_sf_sin(x);
-}
+//// Bessel functions
+//double gsl_bessel_j0(double x) {
+//    return gsl_sf_bessel_J0(x);
+//}
+//
+//double gsl_bessel_j1(double x) {
+//    return gsl_sf_bessel_J1(x);
+//}
+//
+//double gsl_bessel_y0(double x) {
+//    return gsl_sf_bessel_Y0(x);
+//}
+//
+//double gsl_bessel_y1(double x) {
+//    return gsl_sf_bessel_Y1(x);
+//}
+//
+//
+//double gsl_bessel_sin(double x){
+//    return gsl_sf_sin(x);
+//}
