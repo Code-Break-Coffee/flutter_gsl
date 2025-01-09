295d2b0f1b5fe5d14dc8553ea8b2131ddf7fd271
Kartikey2011yadav <nykay10968@gmail.com>
2025-01-09 18:12:38 +0530

    script update

--- src/pythonScript.py
+++ src/pythonScript.py
@@ -1,5 +1,5 @@
 import os
 dir_name=os.path.dirname("./src/")
 files = sorted(os.listdir(dir_name))
-for file in os.listdir(dir_name):
-    print("    - src/"+file)
+for file in files:
+    print("    - \"src/"+file+"\"")
